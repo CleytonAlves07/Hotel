1f3b6ba073f5b4ac3eef5e5f4988d892dc54aa18
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-12-14 16:24:54 -0300

    refactor: :construction: Troca de imagem

--- hotel/src/app/components/banner.tsx
+++ hotel/src/app/components/banner.tsx
@@ -3,18 +3,18 @@ import { HotelForm } from './form';
 
 export function ImgBanner() {
   return (
-    <div className='flex w-screen h-screen '>
+    <div className='flex w-screen h-screen justify-center'>
       <Image
-        src="/images/1-hotel.jpg"
+        src="/images/2-hotel.jpg"
         alt="Hotel banner"
         width={1500}
         height={500}
         className='flex relative mx-auto w-full h-3/4'
       />
-      <div className='flex container absolute mx-auto justify-center items-center h-1/2 w-full lg:w-2/3 md:w-5/6 mt-16' >
+      <div className='flex container absolute mx-auto justify-center items-center h-1/2 w-full lg:w-2/3 md:w-5/6' >
         <div className=' text-center'>
-          <h1 className=' text-yellow-500 md:text-gray-500 lg:text-gray-500 xl:text-gray-500 text-3xl mb-2'>Onde você quer ir?</h1>
-          <p className=' text-gray-600'>Escolha o estado e a cidade para achar os melhores resorts.</p>
+          <h1 className=' text-orange-500 text-3xl font-semibold mb-2'>Onde você quer ir?</h1>
+          <p className=' text-gray-100 font-semibold'>Escolha o estado e a cidade para achar os melhores resorts.</p>
           <HotelForm />
         </div>
 
