5ac8a60175979c65f1aa64df40e7054c851ce156
Cleyton Alves <cleyton.alves.a@gmail.com>
2024-01-02 14:32:34 -0300

    feat: :construction: Incluindo social icons na page contatos

--- hotel/src/app/pages/contatos.tsx
+++ hotel/src/app/pages/contatos.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { HotelHeader } from '../components/header'
+import { SocialIcons } from '../components/socialIcons'
 
 export default function Contatos() {
   return (
     <div>
       <HotelHeader />
-      <div>Contatos </div>
+      <div className=' flex-col justify-center text-center items-center mt-10'>
+        <div className=' font-semibold text-gray-500 mb-4 text-lg'>Contatos </div>
+        <div className=' flex justify-center text-center items-center' ><SocialIcons/></div>
+      </div>
     </div>
   )
 }
